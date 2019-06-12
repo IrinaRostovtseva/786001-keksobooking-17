983b1b724269b7f5c165f283020b64deec14d6d4
IrinaRostovtseva <ni8003@yandex.ru>
2019-06-12 20:48:37 +0300

    Добавляет переменные для отрисовки указателей, функцию для сравнения случайной координаты, заменяет функции для формирования координат объявления

--- js/same_ads.js
+++ js/same_ads.js
@@ -1,17 +1,29 @@
 'use strict';
+
 var map = document.querySelector('.map');
 var MAP_WIDTH = map.offsetWidth;
 var MAP_HEIGHT = map.offsetHeight;
+var mapPinsBlock = document.querySelector('.map__pins');
 var mapPin = document.querySelector('.map__pin');
 var MAP_PIN_WIDTH = mapPin.offsetWidth;
 var MAP_PIN_HEIGHT = mapPin.offsetHeight;
-
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var fragment = document.createDocumentFragment();
 var sameAds = [];
 var accomodationTypes = ['palace', 'flat', 'house', 'bungalo'];
 
 var getRandomNumber = function (maxRange) {
   var randomNumber = Math.floor(Math.random() * maxRange);
   return randomNumber;
+};
+
+var compareNumberWithLimits = function (n, min, max) {
+  if (n < min) {
+    n = min;
+  } else if (n > max) {
+    n = max;
+  }
+  return n;
 };
 
 for (var i = 0; i < 8; i++) {
@@ -23,22 +35,8 @@ for (var i = 0; i < 8; i++) {
       type: accomodationTypes[getRandomNumber(accomodationTypes.length)]
     },
     location: {
-      x: (function () {
-        if (getRandomNumber(MAP_WIDTH) - (MAP_PIN_WIDTH / 2) < 0) {
-          return getRandomNumber(MAP_WIDTH) + (MAP_PIN_WIDTH / 2);
-        } else if (getRandomNumber(MAP_WIDTH) + (MAP_PIN_WIDTH / 2) > MAP_WIDTH) {
-          return getRandomNumber(MAP_WIDTH) - (MAP_PIN_WIDTH / 2);
-        }
-        return getRandomNumber(MAP_WIDTH) - (MAP_PIN_WIDTH / 2);
-      })(),
-      y: (function () {
-        if (getRandomNumber(MAP_HEIGHT) < 130 - MAP_PIN_HEIGHT) {
-          return 130 - MAP_PIN_HEIGHT;
-        } else if (getRandomNumber(MAP_WIDTH) > 630 - MAP_PIN_HEIGHT) {
-          return 630 - MAP_PIN_HEIGHT;
-        }
-        return getRandomNumber(MAP_HEIGHT) - MAP_PIN_HEIGHT;
-      })()
+      x: compareNumberWithLimits(getRandomNumber(MAP_WIDTH) - (MAP_PIN_WIDTH / 2), MAP_PIN_WIDTH / 2, MAP_WIDTH - (MAP_PIN_WIDTH / 2)),
+      y: compareNumberWithLimits((getRandomNumber(MAP_HEIGHT) - MAP_PIN_HEIGHT), 130, 630)
     }
   };
   sameAds[i] = ad;
