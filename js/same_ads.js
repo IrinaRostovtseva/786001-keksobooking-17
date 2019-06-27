7d488289f724a7463b1b91d08e6600d313d75a30
IrinaRostovtseva <ni8003@yandex.ru>
2019-06-27 12:48:26 +0300

    Заменяет mock на данные, полученные с сервера; удаляет неиспользуемые переменные; добавляет функцию для выведения ошибок

--- js/same_ads.js
+++ js/same_ads.js
@@ -1,29 +1,26 @@
 'use strict';
 
 (function () {
-  var MAP_WIDTH = document.querySelector('.map').offsetWidth;
-  var MAP_HEIGHT = document.querySelector('.map').offsetHeight;
-  var MAP_PIN_WIDTH = document.querySelector('.map__pin').offsetWidth;
-  var MAP_PIN_HEIGHT = document.querySelector('.map__pin').offsetHeight;
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var mainBlock = document.querySelector('main');
   window.fragment = document.createDocumentFragment();
-  var sameAds = [];
-  var accomodationTypes = ['palace', 'flat', 'house', 'bungalo'];
 
-  for (var i = 0; i < 8; i++) {
-    var ad = {
-      author: {
-        avatar: 'img/avatars/user' + 0 + (i + 1) + '.png'
-      },
-      offer: {
-        type: accomodationTypes[window.utils.getRandomNumber(accomodationTypes.length)]
-      },
-      location: {
-        x: window.utils.compareNumberWithLimits(window.utils.getRandomNumber(MAP_WIDTH) - (MAP_PIN_WIDTH / 2), MAP_PIN_WIDTH / 2, MAP_WIDTH - (MAP_PIN_WIDTH / 2)),
-        y: window.utils.compareNumberWithLimits((window.utils.getRandomNumber(MAP_HEIGHT) - MAP_PIN_HEIGHT), 130, 630)
-      }
-    };
-    sameAds[i] = ad;
-  }
-  window.utils.createSameElement(sameAds, pinTemplate, window.fragment);
+  var URL = 'https://js.dump.academy/keksobooking/data';
+
+  var onSuccess = function (data) {
+    var sameAds = data;
+    window.utils.createSameElement(sameAds, pinTemplate, window.fragment);
+  };
+  var onError = function (error) {
+    var errorMessage = errorTemplate.cloneNode(true);
+    errorMessage.querySelector('.error__message').textContent = error;
+    mainBlock.appendChild(errorMessage);
+    errorMessage.querySelector('.error__button').addEventListener('click', function (evt) {
+      evt.preventDefault();
+      mainBlock.removeChild(errorMessage);
+      window.recieveData(URL, onSuccess, onError);
+    });
+  };
+  window.recieveData(URL, onSuccess, onError);
 })();
