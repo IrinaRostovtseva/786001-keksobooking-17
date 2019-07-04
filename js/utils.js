c16207077253c789cad9ad5cc071ed427da0ec10
IrinaRostovtseva <ni8003@yandex.ru>
2019-07-04 20:35:05 +0300

    Добавляет функции создания карточки объявления и ее закрытия при нажатии на соответствующую кнопку

--- js/utils.js
+++ js/utils.js
@@ -16,9 +16,13 @@ window.utils = (function () {
     },
     createPin: function (arr, fragment) {
       var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+      var Limit = {
+        TOP: 130,
+        BOTTOM: 630
+      };
       arr.forEach(function (it) {
         var element = pin.cloneNode(true);
-        element.style = 'left: ' + it.location.x + 'px; top: ' + it.location.y + 'px;';
+        element.style = 'left: ' + it.location.x + 'px; top: ' + window.utils.compareNumberWithLimits(it.location.y, Limit.TOP, Limit.BOTTOM) + 'px;';
         element.querySelector('img').src = it.author.avatar;
         element.querySelector('img').alt = it.offer.title;
         fragment.appendChild(element);
@@ -64,6 +68,8 @@ window.utils = (function () {
     onFilterTypeClick: function (data, max) {
       var checked = document.querySelector('#housing-type option:checked');
       var ads;
+      var map = document.querySelector('.map');
+      var adInfoCard = map.querySelector('.map__card');
       if (checked.value !== 'any') {
         ads = data.filter(function (it) {
           return checked.value === it.offer.type;
@@ -72,6 +78,44 @@ window.utils = (function () {
         ads = data.slice(0, max);
       }
       window.utils.createPin(ads, window.fragment);
+      if (adInfoCard) {
+        map.removeChild(adInfoCard);
+      }
+      window.utils.createAdCard(ads[0], window.fragment);
+    },
+    createAdCard: function (adData) {
+      var map = document.querySelector('.map');
+      var filtersBlock = map.querySelector('.map__filters-container');
+      var cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+      var card = cardTemplate.cloneNode(true);
+      card.querySelector('.popup__title').textContent = adData.offer.title;
+      card.querySelector('.popup__text--address').textContent = adData.offer.address;
+      card.querySelector('.popup__text--price').textContent = adData.offer.price + ' ₽/ночь';
+      card.querySelector('.popup__type').textContent = adData.type;
+      card.querySelector('.popup__text--capacity').textContent = adData.offer.rooms + ' комнаты для ' + adData.offer.guests + ' гостей';
+      card.querySelector('.popup__text--time').textContent = 'Заезд после ' + adData.offer.checkin + ', выезд до ' + adData.offer.checkout;
+      card.querySelector('.popup__features').textContent = adData.features;
+      card.querySelector('.popup__description').textContent = adData.offer.description;
+      var adPhotos = card.querySelector('.popup__photos');
+      var photo = adPhotos.querySelector('.popup__photo');
+      adPhotos.removeChild(photo);
+      adData.offer.photos.forEach(function (it) {
+        var img = photo.cloneNode(true);
+        img.src = it;
+        adPhotos.appendChild(img);
+      });
+      card.querySelector('.popup__avatar').src = adData.author.avatar;
+      map.insertBefore(card, filtersBlock);
+      window.utils.closePopup();
+    },
+    closePopup: function () {
+      var map = document.querySelector('.map');
+      var adCard = map.querySelector('.map__card');
+      var popupCloseButton = map.querySelector('.popup__close');
+      popupCloseButton.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        map.removeChild(adCard);
+      });
     }
   };
 })();
