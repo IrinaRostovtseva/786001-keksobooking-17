907d08bbd5c9b56fd82f66e824ef09894a010677
IrinaRostovtseva <ni8003@yandex.ru>
2019-07-06 18:53:18 +0300

    Добавляет пины похожих объявлений при активации карты

--- js/pin_drag.js
+++ js/pin_drag.js
@@ -2,7 +2,8 @@
 
 (function () {
   var map = document.querySelector('.map');
-  var mapPin = document.querySelector('.map__pin--main');
+  var mapPin = map.querySelector('.map__pin--main');
+  var mapPinsBlock = map.querySelector('.map__pins');
   var MAP_PIN_WIDTH = mapPin.offsetWidth;
   var MAP_PIN_HEIGHT = mapPin.offsetHeight;
   var inputsAndSelects = document.querySelectorAll('input, select');
@@ -17,6 +18,12 @@
     TOP: 130,
     BOTTOM: 630
   };
+  var onSuccess = function (data) {
+    var slicedData = data.slice(0, 5);
+    window.utils.createPin(slicedData, window.fragment);
+    mapPinsBlock.appendChild(window.fragment);
+  };
+  
   mapPin.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
     var pinPosition = new MapPinCoordinate(evt.pageX - MAP_PIN_WIDTH * 0.5 - map.offsetLeft, window.utils.compareNumberWithLimits(evt.pageY - MAP_PIN_HEIGHT, Limit.TOP, Limit.BOTTOM));
@@ -25,16 +32,19 @@
       pinPosition = new MapPinCoordinate(moveEvt.pageX - MAP_PIN_WIDTH * 0.5 - map.offsetLeft, window.utils.compareNumberWithLimits(moveEvt.pageY - MAP_PIN_HEIGHT, Limit.TOP, Limit.BOTTOM));
       if (!isActive) {
         window.utils.activateMap(map, adForm, 'map--faded', 'ad-form--disabled', inputsAndSelects);
+        window.recieveData(onSuccess, window.utils.onError);
         isActive = true;
       }
       mapPin.style = 'top: ' + pinPosition.y + 'px;' + 'left: ' + pinPosition.x + 'px;';
       addressField.value = pinPosition.x + ',' + pinPosition.y;
     };
+    
     var onMouseUp = function () {
       mapPin.style = 'top: ' + pinPosition.y + 'px;' + 'left: ' + pinPosition.x + 'px;';
       addressField.value = pinPosition.x + ',' + pinPosition.y;
       if (!isActive) {
         window.utils.activateMap(map, adForm, 'map--faded', 'ad-form--disabled', inputsAndSelects);
+        window.recieveData(onSuccess, window.utils.onError);
         isActive = true;
       }
       map.removeEventListener('mousemove', onMouseMove);
